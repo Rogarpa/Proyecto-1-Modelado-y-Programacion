8c602ae41d49f4498239336b2937544394eb48d2
juan759 <jasso.antonio2001@gmail.com>
2020-11-09 20:32:42 -0600

    Documentación y editada la Interfaz Menu.

--- Menu.java
+++ Menu.java
@@ -4,7 +4,30 @@ public interface Menu {
 	public Iterator<?> createIterator();
 
 	String name;
+
+	/**
+	*Metodo que nos permite acceder al nombre de nuestros platillos.
+	*@return el nombre.
+	*/
 	public String getName() {
 		return name;
 	}
+
+	/**
+	*Método para obtener todas las hojas de nuestra estructura.
+	*@return retorna dos indices, el maximo y el minimo de las hojas.
+	*/
+	public void hijos();
+
+	/**
+	*Metodo que usamos para imprimir la descrpción de un elemento.
+	*@return la descrpción.
+	*/
+	public String toString();
+
+	/**
+	*Método que regresa el indice de un elemento.
+	*@return el indice.
+	*/
+	public int busca();
 }
--- MenuItem.java
+++ MenuItem.java
@@ -5,7 +5,12 @@ public class MenuItem extends MenuComponent {
     String nombre;
     String descriction;
     double precio;
-
+    /**
+    *Constructor de la clase.
+    *@param nombre el nombre del item.
+    *@param descripcion la descriction del item.
+    *@param precio el precio del item.
+    */
     public MenuItem(String nombre,
                     String descripcion,
                     double precio)
@@ -15,18 +20,33 @@ public class MenuItem extends MenuComponent {
         this.price = precio;
     }
 
+    /**
+    *Metodo para acceder al nombre del item.
+    *@return el nombre del item.
+    */
     public String getName() {
         return nombre;
     }
 
+    /**
+    *Metodo para acceder a la descrpción del item.
+    *@return la descrpción del item.
+    */
     public String getDescripcion() {
         return descripcion;
     }
 
+    /**
+    *Metodo para acceder al precio del item.
+    *@return el precio del item.
+    */
     public double getPrecio() {
         return price;
     }
 
+    /**
+    *Metodo que imprime una descritpción detallada del item.
+    */
     public void print() {
         System.out.print("  " + getName());
         System.out.println(", " + getPrecio());
