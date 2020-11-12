18aaefcd8e4674a3e0671254ee19241fd6fca4cd
BlueDevil1164 <73565735+BlueDevil1164@users.noreply.github.com>
2020-11-12 11:10:37 -0600

    Update Pizza.java

--- src/Pizza.java
+++ src/Pizza.java
@@ -5,11 +5,18 @@ public class Pizza implements ElementoMenu{
     protected ElementoMenu[] listaHijos;
 
     public Pizza(){
-        listaHijos = new ElementoMenu[]();
+        listaHijos0 = new ElementoMenu[]("PIZZA DE QUESO","Pizza solo de queso", False, 90, "");
+        listaHijos1 = new ElementoMenu[]("PIZZA VEGANA","Pizza solo de vegetales", False, 100, "");
+        listaHijos2 = new ElementoMenu[]("PIZZA DE DON CANGREJO","La mejor para ti y para mi, es la mejor ", True , 120, "");
+        listaHijos3 = new ElementoMenu[]("PIZZA CLASICA","Pizza solo de queso y peperoni", False, 80, "");
     }
 
     public void agregarHijo(ElementoMenu h){
-        listaHijos.add(h);
+        listaHijos0.add(ListaHijos0);
+        listaHijos1.add(ListaHijos1);
+        listaHijos2.add(ListaHijos2);
+        listaHijos3.add(ListaHijos3);
+        
     }
 
     public void eliminaHIjo(ElementoMenu h){
@@ -18,7 +25,7 @@ public class Pizza implements ElementoMenu{
 
     @Override
     public String toString(){
-        return "";
+        return  listaHijo0"\n" + listaHijo1"\n"  + ListaHijo2"\n" + ListaHijo3"\n";
     }
 
     @Override
