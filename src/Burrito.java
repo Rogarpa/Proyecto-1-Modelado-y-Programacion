f6c41117ceafd5c01a7dabdb3b58ccca00d9dfc4
BlueDevil1164 <73565735+BlueDevil1164@users.noreply.github.com>
2020-11-12 03:08:40 -0600

    Update Burrito.java

--- src/Burrito.java
+++ src/Burrito.java
@@ -6,18 +6,22 @@ public class Burrito implements ElementoMenu{
     protected Hastable<Integer,ElementoMenu> listaHijos;
 
     public Burrito(){
-        listaHijos = new Hashtable<>();
-    }
-
-
+        listaHijos0 = new Hashtable<>("BURRITO DE QUESO" ,"Burrito con relleno de queso" , False , 90 ,"");
+        listaHijos1 = new Hashtable<>("BURRITO DE CARNE" ,"Burrito con exclusivamente de carne" , False , 120 ,"");
+        listaHijos2 = new Hashtable<>("BURRITO VEGANO" ,"Burrito excluisivo de vegetales" , True , 80 ,"");
+        listaHijos3 = new Hashtable<>("BURRITO CLASICO" ,"Burrito sencillo" , True , 70 ,"");
     
     public void agregarHijo(ElementoMenu h){
-        listaHijos.add(h);
+        listaHijos1.add(hijos0);
+        listaHijos1.add(hijos1);
+        listaHijos1.add(hijos2);
+        listaHijos1.add(hijos3);
+        
     }
 
     @Override
     public String toString(){
-        return "";
+        return "hijo0"\n" + hijo1"\n"  + hijo2"\n" + hijo3"\n";
     }
 
     @Override
@@ -30,3 +34,5 @@ public class Burrito implements ElementoMenu{
         return null;
     }
 }
+
+      
