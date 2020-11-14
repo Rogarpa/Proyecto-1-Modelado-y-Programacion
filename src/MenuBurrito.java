4d3732708ecb7ec16f907061d35ba4c5d16dcb21
juan759 <jasso.antonio2001@gmail.com>
2020-11-13 22:06:11 -0600

    Ya imprime mercancias y platillos el menu.

--- src/MenuBurrito.java
+++ src/MenuBurrito.java
@@ -23,24 +23,24 @@ public class MenuBurrito implements Iterable<Producto>{
         this.listaHijos.put("burrito carne",new MenuItem(12,"BURRITO DE CARNE","Burrito con exclusivamente de carne", false, 120.00, TipoProducto.burrito));
         this.listaHijos.put("burrito clasico",new MenuItem(13,"BURRITO CLASICO","Burrito sencillo", false, 70.00, TipoProducto.burrito));
 
-        /**this.listaHijos.put("mercancia1",new Adapter(new Mercancia(1,"PLAYERA ROJA ","Playera",90.00, "Roja","TELA")));
+        this.listaHijos.put("mercancia1",new Adapter(new Mercancia(1,"PLAYERA ROJA ","Playera",90.00, "Roja","TELA")));
         this.listaHijos.put("mercancia2",new Adapter(new Mercancia(2,"PLAYERA AZUL","Playera",90.00, "Azul","TELA")));
-        this.listaHijos.put(16,new Adapter(new Mercancia(3,"PLAYERA VERDE","Playera",90.00, "Verde","TELA")));
-        this.listaHijos.put(17,new Adapter(new Mercancia(4,"PLAYERA ANIMADA(NEGRA)","Playera",100.00, "Negra","TELA")));
-        this.listaHijos.put(18,new Adapter(new Mercancia(5,"PLAYERA ANIMADA(BLANCA)","Playera",100.00, "Blanca","TELA")));
-        this.listaHijos.put(19,new Adapter(new Mercancia(6,"JEANS","Patalones",105.00, "azules","MEZCLILLA")));
-        this.listaHijos.put(20,new Adapter(new Mercancia(7,"PANTALONES DEPORTIVOS ","Pantalones",100.00, "negro","TELA")));
-        this.listaHijos.put(21,new Adapter(new Mercancia(8,"PANTALON FORMAL","Pantalones",100.00, "blanco","TELA")));
-        this.listaHijos.put(22,new Adapter(new Mercancia(9,"CHAMARRA DE CUERO ","Chamarra",120.00, "negro","CUERO")));
-        this.listaHijos.put(23,new Adapter(new Mercancia(10,"CHMARRA SIMPLE","Chamarra",110.00, "verde","TELA")));
-        this.listaHijos.put(24,new Adapter(new Mercancia(11,"CHAMARRA DE MEZCLILLA(Azul)","Chamarra",115.00,"azul","MEZCLILLA")));
-        this.listaHijos.put(25,new Adapter(new Mercancia(12,"CHAMARRA DE MEZCLILLA(Negra)","Chamarra",115.00,"negra","MEZCLILLA")));
-        this.listaHijos.put(26,new Adapter(new Mercancia(13,"SMOKING NEGRO ","Traje",250.00, "negro","TELA")));
-        this.listaHijos.put(27,new Adapter(new Mercancia(14,"SMOKING BLANCO","Traje",250.00, "blanco","TELA")));
-        this.listaHijos.put(28,new Adapter(new Mercancia(15,"SACO(INDIVIDUAL) ","Traje",150.00, "Negro","TELA")));
-        this.listaHijos.put(29,new Adapter(new Mercancia(16,"BUFANDA ","Bufanda",80.00, "Roja","TELA")));
-        this.listaHijos.put(30,new Adapter(new Mercancia(17,"CALCETINES ","Calcetines",50.00, "azules","TELA")));
-        this.listaHijos.put(31,new Adapter(new Mercancia(18,"GORRA ","gorra",60.00, "blancos","TELA")));*/
+        this.listaHijos.put("mercancia3",new Adapter(new Mercancia(3,"PLAYERA VERDE","Playera",90.00, "Verde","TELA")));
+        this.listaHijos.put("mercancia4",new Adapter(new Mercancia(4,"PLAYERA ANIMADA(NEGRA)","Playera",100.00, "Negra","TELA")));
+        this.listaHijos.put("mercancia5",new Adapter(new Mercancia(5,"PLAYERA ANIMADA(BLANCA)","Playera",100.00, "Blanca","TELA")));
+        this.listaHijos.put("mercancia6",new Adapter(new Mercancia(6,"JEANS","Patalones",105.00, "azules","MEZCLILLA")));
+        this.listaHijos.put("mercancia7",new Adapter(new Mercancia(7,"PANTALONES DEPORTIVOS ","Pantalones",100.00, "negro","TELA")));
+        this.listaHijos.put("mercancia8",new Adapter(new Mercancia(8,"PANTALON FORMAL","Pantalones",100.00, "blanco","TELA")));
+        this.listaHijos.put("mercancia9",new Adapter(new Mercancia(9,"CHAMARRA DE CUERO ","Chamarra",120.00, "negro","CUERO")));
+        this.listaHijos.put("mercancia10",new Adapter(new Mercancia(10,"CHMARRA SIMPLE","Chamarra",110.00, "verde","TELA")));
+        this.listaHijos.put("mercancia11",new Adapter(new Mercancia(11,"CHAMARRA DE MEZCLILLA(Azul)","Chamarra",115.00,"azul","MEZCLILLA")));
+        this.listaHijos.put("mercancia12",new Adapter(new Mercancia(12,"CHAMARRA DE MEZCLILLA(Negra)","Chamarra",115.00,"negra","MEZCLILLA")));
+        this.listaHijos.put("mercancia13",new Adapter(new Mercancia(13,"SMOKING NEGRO ","Traje",250.00, "negro","TELA")));
+        this.listaHijos.put("mercancia14",new Adapter(new Mercancia(14,"SMOKING BLANCO","Traje",250.00, "blanco","TELA")));
+        this.listaHijos.put("mercancia15",new Adapter(new Mercancia(15,"SACO(INDIVIDUAL) ","Traje",150.00, "Negro","TELA")));
+        this.listaHijos.put("mercancia16",new Adapter(new Mercancia(16,"BUFANDA ","Bufanda",80.00, "Roja","TELA")));
+        this.listaHijos.put("mercancia17",new Adapter(new Mercancia(17,"CALCETINES ","Calcetines",50.00, "azules","TELA")));
+        this.listaHijos.put("mercancia18",new Adapter(new Mercancia(18,"GORRA ","gorra",60.00, "blancos","TELA")));
 
 
     }
